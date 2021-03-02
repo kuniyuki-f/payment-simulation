9bc6aa6f71b81776946548f40f55fecc40f07257
c501306014 <c501306014@gmail.com>
2021-03-02 20:24:13 +0900

    add main range

---
+++ src/components/range.vue
@@ -0,0 +1,37 @@
+<template>
+  <div>
+    <input type="range" v-model="innerVal" :min="min" :max="max" />
+    <vue-slider v-model="innerVal" :min="min" :max="max" />
+  </div>
+</template>
+
+<script>
+export default {
+  name: "myRange",
+  props: {
+    value: { type: Number, required: true, default: 0 },
+    min: { type: Number, required: true, default: 0 },
+    max: { type: Number, required: true, default: 0 },
+    step: { type: Number, required: true, default: 0 },
+  },
+  data: function () {
+    return {
+      dataValue: this.value,
+    };
+  },
+  computed: {
+    innerVal: {
+      get() {
+        return this.value;
+      },
+      set(value) {
+        value = Number(value);
+        this.$emit("update:value", value);
+      },
+    },
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+</style>
