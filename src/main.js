6a4955b8dd70fc2e4665e1aaca95cb3bb748222d
c501306014 <c501306014@gmail.com>
2021-03-02 20:43:28 +0900

    remove bootstrap

--- src/main.js
+++ src/main.js
@@ -3,19 +3,11 @@ import App from './App.vue'
 
 
 
-import {
-  BootstrapVue,
-  BootstrapVueIcons
-} from 'bootstrap-vue'
-import 'bootstrap/dist/css/bootstrap.css'
-import 'bootstrap-vue/dist/bootstrap-vue.css'
 import vuetify from './plugins/vuetify';
 
 Vue.config.productionTip = false
 
 
-Vue.use(BootstrapVue)
-Vue.use(BootstrapVueIcons)
 
 new Vue({
   vuetify,
